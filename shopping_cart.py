416d538b993428e8a7bea69c2bf8f126fefb0f5a
areggev <acr644@stern.nyu.edu>
2021-06-14 14:10:53 -0400

    Update Total Price

--- shopping_cart.py
+++ shopping_cart.py
@@ -67,7 +67,7 @@ for selected_id in selected_ids:
 
         matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
         matching_product = matching_products[0]
-        total_price = total_ price + matching_product["price"]
+        total_price = total_price + matching_product["price"]
         print("...", matching_product["name"], "(" , to_usd(matching_product["price"]), ")")
 
 
